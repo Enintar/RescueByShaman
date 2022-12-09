59896053544c6f979297bcc64792e40ef9ccc731
Enintar <67773497+Enintar@users.noreply.github.com>
2022-12-09 13:20:03 +0100

    fixed

---
+++ src/main/java/shaman/rescue/commands/RescueCommand.java
@@ -0,0 +1,47 @@
+package shaman.rescue.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import shaman.rescue.Rescue;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class RescueCommand implements org.bukkit.command.CommandExecutor {
+    public static final String COMMAND_CONSOLE = ChatColor.translateAlternateColorCodes('&', "&cRescue &7| &fДанную команду можно выполнять только с &cконсоли&f!");
+    public static final String COMMAND_USAGE = ChatColor.translateAlternateColorCodes('&', "&cRescue &7| &fИспользуйте: &c/rescue [ник]");
+    public static final String COMMAND_SUCCESS_ONLINE = ChatColor.translateAlternateColorCodes('&', "&cRescue &7| &fВы успешно телепортировали &c%s &fна спавн!");
+    public static final String COMMAND_NO_PLAYER = ChatColor.translateAlternateColorCodes('&', "&cRescue &7| &fИгрок с ником &c%s &fне существует!");
+    public static final String COMMAND_ALREADY = ChatColor.translateAlternateColorCodes('&', "&cRescue &7| &fИгрок с ником &c%s &fуже есть в очереди!");
+    public static final String COMMAND_SUCCESS_OFFLINE = ChatColor.translateAlternateColorCodes('&', "&cRescue &7| &fВы успешно добавили &c%s &fв очередь!");
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Rescue rescue = Rescue.getInstance();
+        if (sender instanceof Player && rescue.isConsoleOnly()) {
+            sender.sendMessage(COMMAND_CONSOLE);
+            return true;
+        }
+        if (args.length == 0) {
+            sender.sendMessage(COMMAND_USAGE);
+            return true;
+        }
+        OfflinePlayer a = getServer().getOfflinePlayer(args[0]);
+        if (a.isOnline()) {
+            rescue.spawnTeleport(a.getPlayer());
+            sender.sendMessage(String.format(COMMAND_SUCCESS_ONLINE, a.getName()));
+            return true;
+        }
+        if (!a.hasPlayedBefore()) {
+            sender.sendMessage(String.format(COMMAND_NO_PLAYER, args[0]));
+            return true;
+        }
+        if (rescue.getTeleportNames().add(a.getName())) {
+            sender.sendMessage(String.format(COMMAND_SUCCESS_OFFLINE, a.getName()));
+        } else {
+            sender.sendMessage(String.format(COMMAND_ALREADY, a.getName()));
+        }
+        return true;
+    }
+}
